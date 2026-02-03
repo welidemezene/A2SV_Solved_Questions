90b02f85fd857b6ca9a3bcb90c08a90f2b5851e6
Woldemedihn <woldemedihnmezene@gmail.com>
2026-02-03 10:48:52 +0300

    Time: 0 ms (100%), Space: 19.1 MB (27.89%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = ""
-        lis = []
-        for l in digits:
-            s += str(l)
-        n = int(s)
-        n+=1
-        k = str(n)
-        for m in k:
-            lis.append(int(m))
-        return lis    
-              
+        list1 = []
+        for n in digits:
+            list1.append(str(n))
+        list2 = "".join(list1) 
+        list3 = int(list2)
+        list3+=1
+        list4 = str(list3)
+        list5 = []
 
+        for s in list4:
+            list5.append(int(s))
+        return list5    
+
+
+       
+
+      
+        
         
