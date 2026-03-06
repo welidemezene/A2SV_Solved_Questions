3c1a4a7c18cfda0aab090f79372d9ce77f7a5196
Woldemedihn <woldemedihnmezene@gmail.com>
2026-03-06 20:22:17 +0300

    Time: 7 ms (55.86%), Space: 19.7 MB (50.18%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,21 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        le = len(heights)
-        freq = defaultdict(str)
-        for i in range(le):
-            freq[heights[i]] = names[i]
-        check = False
-        
-        for i in range(le):
-            for j in range(i+1,le):
-                if heights[j] > heights[i]:
-                    heights[i],heights[j] = heights[j],heights[i]
-                    
-           
-        li = []        
-        for n in heights:   
-            li.append(freq[n]) 
-        return li                
-
-
-        
+        h = len(heights)
+        fre1 = defaultdict(str)
+        for i in range(h):
+            fre1[heights[i]] = names[i]
+        heights.sort(reverse=True)
+        stack = []
+        for num in heights:
+            stack.append(fre1[num])
+        return stack        
