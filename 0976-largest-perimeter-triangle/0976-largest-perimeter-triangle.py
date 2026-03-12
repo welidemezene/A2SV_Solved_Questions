2daa5a63f91d187bb0d05fde3668f194a8e308ed
Woldemedihn <woldemedihnmezene@gmail.com>
2026-03-12 18:26:38 +0300

    Time: 10 ms (86.04%), Space: 20.3 MB (99.15%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -1,14 +1,11 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort(reverse=True)
-        max1 = 0
-        for i in range(len(nums)-2):
-            if nums[i] < nums[i+1]+nums[i+2]:
-                return nums[i]+nums[i+1] +nums[i+2]
-               
-        return 0
-
-
-
-
+        le = len(nums)
+        for i in range(le-2):
+            sum = nums[i+1]+nums[i+2]
+            if nums[i] < sum:
+                return nums[i]+sum
+        else:
+            return 0        
         
