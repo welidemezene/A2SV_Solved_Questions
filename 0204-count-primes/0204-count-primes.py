a6d7ad806db54bc29298c1612271e9a796ead976
Woldemedihn <woldemedihnmezene@gmail.com>
2026-08-31 07:51:30 +0300

    Time: 9477 ms (10.41%), Space: 85.4 MB (29.63%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
+        is_prime = [True] * n
+        is_prime[0] = False
+        is_prime[1] = False
+        for i in range(2,int(n**0.5)+1):
+            if is_prime[i]:
+                for mult in range(i*i, n,i):
+                    is_prime[mult] = False
+        return sum(is_prime)            
+        
