cce4ca042c3e6ce8758b62aec3da2faed52186a3
Woldemedihn <woldemedihnmezene@gmail.com>
2026-09-04 23:12:29 +0300

    Time: 4 ms (4.68%), Space: 19.1 MB (90.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,15 +1,12 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        count = {}
+        for i in range(len(nums)):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+        
+        idx = 0
 
-        red, white, blue = 0, 0, len(nums) - 1
-
-        while white <= blue:
-            if nums[white] == 0:
-                nums[white], nums[red] = nums[red], nums[white]
-                red += 1
-                white += 1
-            elif nums[white] == 1:
-                white += 1
-            else:
-                nums[white], nums[blue] = nums[blue], nums[white]
-                blue -= 1
+        for color in range(3):
+            freq = count.get(color, 0)
+            nums[idx : idx + freq] = [color] * freq
+            idx += freq
